4dd22008e4939dfd3a4bb6aedc7fa0a1523f4506
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-03-31 16:57:41 -0700

    re-indent

--- structs.h
+++ structs.h
@@ -6,12 +6,13 @@
 typedef int boolean;
 
 typedef struct ssector {
-        int enemy, star, tf;
+    int enemy, star, tf;
 } sector;
 
-typedef struct ssector tboard[bdsize+1][bdsize+1];
+typedef struct ssector tboard[bdsize + 1][bdsize + 1];
 
 typedef int tteam;
+
 #define ENEMY   0
 #define player  1
 #define none    2
@@ -19,40 +20,39 @@ typedef int tteam;
 typedef int attribute[2];
 
 typedef struct sttf {
-                int x,y,xf,yf,
-                s,t,c,b,
-                dest,eta,origeta;
-                int blasting, withdrew;
+    int x, y, xf, yf, s, t, c, b, dest, eta, origeta;
+    int blasting, withdrew;
 } ttf;
 
 typedef struct ststar {
-        int x, y;
-        struct stplanet *first_planet;
-        int visit[2];
+    int x, y;
+    struct stplanet *first_planet;
+    int visit[2];
 } tstar;
 
 typedef struct stplanet {
-                int number,capacity,psee_capacity;
-                int team;
-                int inhabitants,iu,mb,amb;
-                int conquered,under_attack;
-                int esee_team;  /*the   team when the enemy last saw it*/
-                int esee_def; /*the     mbs when enemy last saw it*/
-                int pstar;
-                struct stplanet *next;
+    int number, capacity, psee_capacity;
+    int team;
+    int inhabitants, iu, mb, amb;
+    int conquered, under_attack;
+    int esee_team;              /*the team when the enemy last saw it */
+    int esee_def;               /*the mbs when enemy last saw it */
+    int pstar;
+    struct stplanet *next;
 } tplanet;
 
 typedef struct sttf tfray[2][27];
 
-typedef struct ststar starray[nstars+1];
+typedef struct ststar starray[nstars + 1];
 
 typedef char tline[81];
 
 typedef char alfa[6];
 
-typedef float starlist[nstars+1];
+typedef float starlist[nstars + 1];
 
 typedef int toption;
+
 #define right   0
 #define left    1
 #define both    2
--- vars.h
+++ vars.h
@@ -1,30 +1,26 @@
 #ifndef VARS_H
 #define VARS_H
 
-extern int r2nge[nstars+1][nstars+1],
-           tf_stars[nstars+1][2],
-           col_stars[nstars+1][2];
+extern int r2nge[nstars + 1][nstars + 1],
+    tf_stars[nstars + 1][2], col_stars[nstars + 1][2];
 
 extern char en_research;
 
-extern sector board[bdsize+1][bdsize+1];
+extern sector board[bdsize + 1][bdsize + 1];
 
-extern tstar stars[nstars+1];
+extern tstar stars[nstars + 1];
 
 extern ttf tf[2][27];
 
 extern float growth_rate[2];
 
-extern int vel[2],range[2],weapons[2],
-           weap_working[2],ran_working[2],vel_working[2],
-           weap_req[11], ran_req[bdsize+1],
-           vel_req[max_vel+1], turn,production_year,
-           enemy_arrivals[nstars+1], en_departures[nstars+1],
-           player_arrivals[nstars+1], game_over,
-           bottom_field;
+extern int vel[2], range[2], weapons[2],
+    weap_working[2], ran_working[2], vel_working[2],
+    weap_req[11], ran_req[bdsize + 1],
+    vel_req[max_vel + 1], turn, production_year,
+    enemy_arrivals[nstars + 1], en_departures[nstars + 1],
+    player_arrivals[nstars + 1], game_over, bottom_field;
 
-extern int x_cursor, y_cursor,
-           saved_game,
-           left_line[25], debug;
+extern int x_cursor, y_cursor, saved_game, left_line[25], debug;
 
 #endif
